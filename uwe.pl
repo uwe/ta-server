8939f949bbb4c6ec08a9a811b81e892ccdf275f1
Uwe <uwe@uwevoelker.de>
2012-04-15 16:33:01 +0200

    use two arrays for inout bars

--- uwe.pl
+++ uwe.pl
@@ -8,17 +8,20 @@ use FinanceChart;
 use TA::Server;
 
 
+my $symbol = $ARGV[0] || 'GOOG';
+my $days   = $ARGV[1] || 100;
+
+
 my $ta = TA::Server->new;
 
+my $data = $ta->get_quotes($symbol);
 
-my $data = $ta->get_quotes($ARGV[0] || 'GOOG');
+my @data = splice(@$data, -$days);
 
-my @data = splice(@$data, - ($ARGV[1] || 100));
-
-add_aussenstab(@data);
+add_inout_bars(@data);
 
 my $chart = new FinanceChart(1200);
-$chart->addTitle("Finance Chart Demonstration");
+$chart->addTitle($symbol);
 $chart->setData(
     [ map { perlchartdir::chartTime(split /[- :]/, $_->{date}) } @data ],
     [ map { $_->{high}   } @data ],
@@ -31,44 +34,54 @@ $chart->setData(
 
 my $main = $chart->addMainChart(800);
 $chart->addHLOC(0x008000, 0xcc0000)->setLineWidth(2);
-$chart->addLineIndicator2($main, [ map { $_->{inout_high} } @data ], 0x888888, 'InOut High');
-$chart->addLineIndicator2($main, [ map { $_->{inout_low}  } @data ], 0x888888, 'InOut Low');
+$chart->addLineIndicator2($main, [ map { $_->{inout_high_0} } @data ], 0x888888, 'InOut High');
+$chart->addLineIndicator2($main, [ map { $_->{inout_low_0}  } @data ], 0x888888, 'InOut Low');
+$chart->addLineIndicator2($main, [ map { $_->{inout_high_1} } @data ], 0x888888, 'InOut High');
+$chart->addLineIndicator2($main, [ map { $_->{inout_low_1}  } @data ], 0x888888, 'InOut Low');
 $chart->makeChart("out.png");
 
 
-sub add_aussenstab {
+sub add_inout_bars {
     my @data = @_;
 
-    my $aussenstab;
+    my $index = 0;
+    my $inout;
     my $last;
     foreach my $day (@data) {
-        if ($aussenstab) {
-            unless (check_innenstab($aussenstab, $day)) {
-                $aussenstab = undef;
+        if ($inout) {
+            unless (check_inbar($inout, $day, $index)) {
+                # draw breakout
+                $day->{"inout_high_$index"} = $inout->{high};
+                $day->{"inout_low_$index"}  = $inout->{low};
+
+                $inout = undef;
+                $index = 1 - $index;
             }
         }
         else {
-            if ($last and check_innenstab($last, $day)) {
-                $aussenstab = $last;
-                $last->{inout_high} = $last->{high};
-                $last->{inout_low}  = $last->{low}; 
+            if ($last and check_inbar($last, $day, $index)) {
+                $inout = $last;
+                $last->{"inout_high_$index"} = $last->{high};
+                $last->{"inout_low_$index"}  = $last->{low}; 
             }
         }
 
-        $day->{inout_high} ||= $perlchartdir::NoValue;
-        $day->{inout_low}  ||= $perlchartdir::NoValue;
+        $day->{inout_high_0} ||= $perlchartdir::NoValue;
+        $day->{inout_high_1} ||= $perlchartdir::NoValue;
+        $day->{inout_low_0}  ||= $perlchartdir::NoValue;
+        $day->{inout_low_1}  ||= $perlchartdir::NoValue;
 
         $last = $day;
     }
 }
 
-sub check_innenstab {
-    my ($aussen, $innen) = @_;
+sub check_inbar {
+    my ($out, $in, $index) = @_;
     
-    if ($innen->{open}  < $aussen->{high} and $innen->{open}  > $aussen->{low} and
-        $innen->{close} < $aussen->{high} and $innen->{close} > $aussen->{low}) {
-        $innen->{inout_high} = $aussen->{high};
-        $innen->{inout_low}  = $aussen->{low};
+    if ($in->{open}  < $out->{high} and $in->{open}  > $out->{low} and
+        $in->{close} < $out->{high} and $in->{close} > $out->{low}) {
+        $in->{"inout_high_$index"} = $out->{high};
+        $in->{"inout_low_$index"}  = $out->{low};
         return 1;
     }
 
